c9e231cbd7f8e7eb8956b562482783492f72cf70
naitsircnat <cristiangerardchen@gmail.com>
2024-12-03 09:46:59 +0000

    Completed lab 3

--- index.js
+++ index.js
@@ -14,13 +14,6 @@ app.use(express.json());
 // !! Enable CORS
 app.use(cors());
 
-// 2. CREATE ROUTES
-// app.get("/", function (req, res) {
-//   res.json({
-//     message: "Hello World!",
-//   });
-// });
-
 async function connect(uri, dbname) {
   let client = await MongoClient.connect(uri);
   let db = client.db(dbname);
@@ -39,12 +32,8 @@ async function main() {
   let db = await connect(mongoUri, dbname);
 
   // Routes
-  // app.get("/", function (req, res) {
-  //   res.json({
-  //     message: "Hello World!",
-  //   });
-  // });
 
+  // display recipes
   app.get("/recipes", async (req, res) => {
     try {
       const recipes = await db
@@ -61,6 +50,71 @@ async function main() {
       res.json({ recipes });
     } catch (error) {
       console.error("Error fetching recipes:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  // display recipe of a certain ID
+  const { ObjectId } = require("mongodb");
+
+  app.get("/recipes/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+
+      // First, fetch the recipe
+      const recipe = await db
+        .collection("recipes")
+        .findOne({ _id: new ObjectId(id) }, { projection: { _id: 0 } });
+
+      if (!recipe) {
+        return res.status(404).json({ error: "Recipe not found" });
+      }
+
+      res.json(recipe);
+    } catch (error) {
+      console.error("Error fetching recipe:", error);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  });
+
+  // perform search
+  app.get("/recipes", async (req, res) => {
+    try {
+      const { tags, cuisine, ingredients, name } = req.query;
+      let query = {};
+
+      if (tags) {
+        query["tags.name"] = { $in: tags.split(",") };
+      }
+
+      if (cuisine) {
+        query["cuisine.name"] = { $regex: cuisine, $options: "i" };
+      }
+
+      if (ingredients) {
+        query["ingredients.name"] = {
+          $all: ingredients.split(",").map((i) => new RegExp(i, "i")),
+        };
+      }
+
+      if (name) {
+        query.name = { $regex: name, $options: "i" };
+      }
+
+      const recipes = await db
+        .collection("recipes")
+        .find(query)
+        .project({
+          name: 1,
+          "cuisine.name": 1,
+          "tags.name": 1,
+          _id: 0,
+        })
+        .toArray();
+
+      res.json({ recipes });
+    } catch (error) {
+      console.error("Error searching recipes:", error);
       res.status(500).json({ error: "Internal server error" });
     }
   });
